3f94764f20f2b0c7232758e152a68e7d5ebc0502
madnavs <nmadira1@asu.edu>
2017-04-01 20:06:11 -0700

    solver update

--- caffe_skeleton_ready/run.sh
+++ caffe_skeleton_ready/run.sh
@@ -11,23 +11,30 @@ MODEL=trained_models/*.caffemodel
 #rm $MODEL
 #cp pretrained/$MODEL trained_models/
 #=trained_models/$MODEL
-NUM_LABELS=2
 
-convert_imageset -resize_height 256 -resize_width 256 -shuffle / List/train_list.txt train_image_lmdb
+convert_imageset  -shuffle / List/train_list.txt train_image_lmdb
 compute_image_mean train_image_lmdb/ train_image_mean.binaryproto
 
-convert_imageset -resize_height 256 -resize_width 256  / List/valid_list.txt val_image_lmdb
+convert_imageset   / List/valid_list.txt val_image_lmdb
 
-convert_imageset -resize_height 256 -resize_width 256  / List/test_list.txt test_image_lmdb
+convert_imageset   / List/test_list.txt test_image_lmdb
 
 #----------------set solver properties ----------------#
+batchsize=64 ##Chage this!!
+epoch=10
+NUM_LABELS=2
+##-------------val list --------------------
+n_val_patches=$(wc -l <  List/valid_list.txt)
+iter_val=$((n_val_patches/batchsize+1))
+sed -i "s/^test_iter.*$/test_iter: $iter_val/" solver.prototxt
+
+##--------------Test List Prop -------------
 n_test_patches=$(wc -l <  List/test_list.txt)
-iter_test=$n_test_patches/64+1
+iter_test=$((n_test_patches/batchsize+1))
 N_train=$(cat List/train_list.txt | wc -l)
-batchsize=64
 stepsize=$((N_train/batchsize))
 echo "# of train patch=$N_train. based on batchsize=$batchsize, step size is $stepsize"
-epoch=6
+
 max_iter=$(($stepsize * $epoch))
 echo "Updating solver.prototxt with stepsize of $stepsize"
 sed -i "s/^stepsize.*$/stepsize: $stepsize/" solver.prototxt
@@ -38,12 +45,13 @@ sed -i "s/^snapshot:.*$/snapshot: $max_iter/" solver.prototxt
 screen -dm bash -c 'sleep 5; sh visualize_log.sh;exec sh'
 caffe train -solver solver.prototxt -weights ../Step0/$MODEL  2>&1 | tee log/caffe_train_print.txt 
 
+echo "using this model" $MODEL
 extract_features $MODEL test.prototxt output_3 test_features_output $iter_test lmdb GPU DEVICE_ID=0
 
 python aggregate_lmdb.py List/test_list.txt test_features_output test.csv
 
 if [ $NUM_LABELS = 2 ]; then
-               matlab -nodesktop -nosplash -r "Accuracy();quit"
+               matlab -nodesktop -nosplash -r "plot_aggregate();quit"
             else
                echo expression evaluated as false
             fi
